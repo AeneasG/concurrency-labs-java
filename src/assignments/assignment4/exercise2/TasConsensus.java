8d084776c547da2f7d72c05b430d44255452e58e
Aeneas Grueter <aeneas.grueter@gmail.com>
2022-04-29 21:15:44 +0200

    Assignment 4, Exercise 2 - Fix TasConsensus to not use a string as initial value because strings are shared throughout the system

--- src/assignments/assignment4/exercise2/TasConsensus.java
+++ src/assignments/assignment4/exercise2/TasConsensus.java
@@ -4,11 +4,14 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class TasConsensus implements IConsensus{
 
+    private class NullObject {
+    }
+
     AtomicReference<Object> decision;
     Object nullObject;
 
     public TasConsensus() {
-        this.nullObject = "";
+        this.nullObject = new NullObject();
         this.decision = new AtomicReference<Object>(this.nullObject);
     }
 
